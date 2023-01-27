32b62673c94b36281c43211868f1c7b2a2506060
nreed31415 <91569412+nreed31415@users.noreply.github.com>
2023-01-27 07:46:18 -0700

    Update star.h
    not finished yet?

--- star.h
+++ star.h
@@ -9,11 +9,27 @@
 
 #pragma once
 
+#include "point.h"   // for Point
+#include "uiDraw.h"   // for ogstream
+
 /*********************************************
  * STAR
  * A single star
  *********************************************/
 class Star
 {
+private:        //Member variables needed for drawing the star
+   Point pt;    //The position of a star
+   char phase;  //The phase of the star
+
+public:
+   //the constructor generates a star
+   Star(const Point& pt);
+
+   //reset the game
+   void reset(int width, int height);
+
+   //draw the star on the screen 
+   void draw(ogstream & gout);
 
 };
